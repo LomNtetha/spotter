e49814fad9a27925a1bfaa8f14d8bdab09a418e8
Lumkile Ntetha <ntethalumkile@gmail.com>
2025-03-18 07:31:32 +0200

    register app in defalt dashboard for better management

--- transport/admin.py
+++ transport/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Location, ELDLog, Trip
 
-# Register your models here.
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'latitude', 'longitude')
+    search_fields = ('name',)
+
+@admin.register(ELDLog)
+class ELDLogAdmin(admin.ModelAdmin):
+    list_display = ('id', 'hours_used', 'timestamp')
+    list_filter = ('timestamp',)
+    search_fields = ('hours_used',)
+
+@admin.register(Trip)
+class TripAdmin(admin.ModelAdmin):
+    list_display = ('id', 'current_location', 'pickup_location', 'dropoff_location', 'cycle_hours_used', 'fuel_needed')
+    list_filter = ('fuel_needed',)
+    search_fields = ('current_location__name', 'pickup_location__name', 'dropoff_location__name')
